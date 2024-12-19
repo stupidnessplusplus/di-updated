f1e7a4470dee8699ef3e1408b941fb312475f284
stupidnessplusplus <58785209+stupidnessplusplus@users.noreply.github.com>
2024-12-19 23:46:47 +0500

    Add tag classes

---
+++ TagsCloudCreation/TagDrawing.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace TagsCloudCreation;
+
+public record TagDrawing(
+    Tag Tag,
+    Brush Brush,
+    string FontName,
+    FontStyle FontStyle)
+{
+    public void Draw(Graphics graphics)
+    {
+        using var font = new Font(FontName, Tag.Rectangle.Height, FontStyle, GraphicsUnit.Pixel);
+        graphics.DrawString(Tag.Word, font, Brush, Tag.Rectangle.Location);
+    }
+}
