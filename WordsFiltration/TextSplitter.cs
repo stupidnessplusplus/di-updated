897955eadd49db0fd3bf188228b1dcc906c2f520
stupidnessplusplus <58785209+stupidnessplusplus@users.noreply.github.com>
2024-12-19 23:53:37 +0500

    Add TextSplitter

---
+++ WordsFiltration/TextSplitter.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WordsFiltration;
+
+public class TextSplitter
+{
+    public string[] SplitToWords(string text)
+    {
+        text = text.ToLower();
+        return Regex.Split(text, @"[\p{P}\s]+");
+    }
+}
