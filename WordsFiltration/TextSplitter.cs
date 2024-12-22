e3f7dc67b527423aff256b8e72a005b67624164b
stupidnessplusplus <58785209+stupidnessplusplus@users.noreply.github.com>
2024-12-23 04:20:28 +0500

    Add IWordsSelector, change TextSplitter

--- WordsFiltration/TextSplitter.cs
+++ WordsFiltration/TextSplitter.cs
@@ -1,12 +1,29 @@
 ﻿using System.Text.RegularExpressions;
+using WordsFiltration.WordsSelectors;
 
 namespace WordsFiltration;
 
 public class TextSplitter
 {
+    private static readonly Regex _wordSplitRegex = new Regex(@"[\p{P}\s-[-]]+");
+
+    private readonly IEnumerable<IWordsSelector> _wordsSelectors;
+
+    public TextSplitter(IEnumerable<IWordsSelector> wordsSelectors)
+    {
+        _wordsSelectors = wordsSelectors;
+    }
+
     public string[] SplitToWords(string text)
     {
         text = text.ToLower();
-        return Regex.Split(text, @"[\p{P}\s]+");
+
+        var words = _wordSplitRegex
+            .Split(text)
+            .Where(word => !string.IsNullOrEmpty(word) && !word.All(ch => ch == '-'));
+
+        return _wordsSelectors
+            .Aggregate(words, (words, wordsSelector) => wordsSelector.Select(words))
+            .ToArray();
     }
 }
