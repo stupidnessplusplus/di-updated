79284ad07682ccaa8ace39d7055cbeb0f51de5e6
stupidnessplusplus <58785209+stupidnessplusplus@users.noreply.github.com>
2024-12-23 07:56:35 +0500

    Add words filtration tests

---
+++ WordsFiltration_Tests/WordsSelectors/WordsSelectorTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using WordsFiltration.WordsSelectors;
+
+namespace WordsFiltration_Tests.WordsSelectors;
+
+internal abstract class WordsSelectorTests
+{
+    protected IWordsSelector wordSelector = null!;
+
+    [Test]
+    public void Select_ThrowsException_WhenWordsEnumerableIsNull()
+    {
+        var decorate = () => wordSelector.Select(null!);
+        decorate.Should().Throw<ArgumentNullException>();
+    }
+}
